6697f3189b35d629f3e084dfa7dfee506f5e0d53
Tatsuro Shibamura <me@shibayan.jp>
2021-04-13 00:29:40 +0900

    Update for Microsoft Build of OpenJDK

--- content/install.cmd
+++ content/install.cmd
@@ -3,19 +3,12 @@
 :: Setup
 :: ----------------
 
-set JAVA_VERSION=jdk1.8.0_74
-set EXTENSION_HOME=%HOME%\SiteExtensions\java
+set JAVA_VERSION=jdk-11.0.10.9
+set EXTENSION_HOME=%HOME%\SiteExtensions\Java
 
 :: Installing Java
-curl -b "oraclelicense=accept-securebackup-cookie" -LOs http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-windows-i586.exe
-7z x jdk-8u74-windows-i586.exe -o%EXTENSION_HOME%
-7z x %EXTENSION_HOME%\tools.zip -o%EXTENSION_HOME%\%JAVA_VERSION%
-
-:: Unpacking
-pushd %JAVA_VERSION%
-for /r %%x in (*.pack) do .\bin\unpack200 -r "%%x" "%%~dx%%~px%%~nx.jar"
-popd
+curl -LOs https://aka.ms/download-jdk/microsoft-%JAVA_VERSION%-windows-x64.zip
+7z x microsoft-%JAVA_VERSION%-windows-x64.zip
 
 :: Cleanup
-rm tools.zip
-rm jdk-8u74-windows-i586.exe
+rm microsoft-%JAVA_VERSION%-windows-x64.zip
